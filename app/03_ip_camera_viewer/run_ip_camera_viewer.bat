4fced5fe5dcf8063acc9383d33738f5879cfea31
RoadLabo <info@roadlabo.com>
2026-03-28 13:30:38 +0900

    Update run_ip_camera_viewer.bat

--- app/03_ip_camera_viewer/run_ip_camera_viewer.bat
+++ app/03_ip_camera_viewer/run_ip_camera_viewer.bat
@@ -5,7 +5,7 @@ cd /d "C:\_TsuyamaSignage\app\03_ip_camera_viewer"
 
 echo [INFO] Starting IP Camera Viewer...
 
-"C:\_PythonEnvs\venv310_ipcam\Scripts\python.exe" ^
+"C:\_PythonEnvs\venv_ip_camera_viewer\Scripts\python.exe" ^
 "C:\_TsuyamaSignage\app\03_ip_camera_viewer\ip_camera_viewer.py" ^
 >> "C:\_TsuyamaSignage\logs\ip_camera_viewer.log" 2>&1
 
